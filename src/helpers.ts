c0e0469b97db6ece64e9ebccdb99212048ad074d
Law Horne <hello@foxtrottwist.com>
2024-09-11 09:02:39 -0400

    Added List and Node classes to store binary time.

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,47 @@
-export function decimalToBinary(decimal: number, remainders: number[] = []) {
+export class Node<T> {
+  data: T;
+  next?: Node<T>;
+
+  constructor(value: T) {
+    this.data = value;
+  }
+}
+
+export class List<T> {
+  head?: Node<T>;
+  tail?: Node<T>;
+  length = 0;
+
+  prepend(value: T) {
+    const node = new Node(value);
+
+    if (this.head) {
+      node.next = this.head;
+      this.head = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+
+    this.length++;
+  }
+
+  clear() {
+    this.tail = undefined;
+
+    while (this.head) {
+      this.head = this.head.next;
+      this.length--;
+    }
+  }
+}
+
+export function decimalToBinary(
+  decimal: number,
+  remainders: List<number> = new List(),
+) {
   const quotient = Math.floor(decimal / 2);
-  remainders.push(decimal % 2);
+  remainders.prepend(decimal % 2);
 
   if (quotient === 0) {
     return remainders;
@@ -10,9 +51,14 @@ export function decimalToBinary(decimal: number, remainders: number[] = []) {
 }
 
 export function getBinaryTime() {
-  const now = new Date();
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+
   return {
-    hours: decimalToBinary(now.getHours()),
-    minutes: decimalToBinary(now.getMinutes()),
+    decimalHours: hours,
+    decimalMinutes: minutes,
+    hours: decimalToBinary(date.getHours()),
+    minutes: decimalToBinary(date.getMinutes()),
   };
 }
